1f66a0be7898734864d3a658b46bd1babd63ec70
GlitchedMastery <78371022+OnTheHubLol@users.noreply.github.com>
2023-01-25 18:01:11 +0000

    New Functions Update
    Added Square Rooting, Prime Numbers and more!

--- calc.py
+++ calc.py
@@ -1,7 +1,6 @@
-
+import math
 def add(x, y):
     return x + y
-
 
 def subtract(x, y):
     return x - y
@@ -20,6 +19,20 @@ def squared(x):
 def cubed(x):
     return x * x * x
 
+def squareroot(x):
+    return math.sqrt(x)
+
+def cuberoot(x):
+    return math.cbrt(x)
+
+def isprime(n):
+  for i in range(2,int(math.sqrt(n))+1):
+    if (n%i) == 0:
+      return str("Is Not A Prime Number")
+  return str("Is A Prime Number")
+
+def tothepowerof(x, y):
+    return x ** y
 
 print("Select an operation.")
 print("1.Add")
@@ -28,10 +41,13 @@ print("3.Multiply")
 print("4.Divide")
 print("5.Square A Number")
 print("6.Cube A Number")
-
+print("7.Square Root A Number")
+print("8.Cube Root A Number")
+print("9.Find Out If A Number Is Prime")
+print("10.Power Numbers")
 
 while True:
-    choice = input("Enter choice(1/2/3/4/5/6): ")
+    choice = input("Enter choice(1/2/3/4/5/6/7/8/9/10): ")
 
     if choice in ('1', '2', '3', '4'):
         try:
@@ -72,10 +88,56 @@ while True:
 
         if choice == '6':
             print(num1, "*", num1, "*", num1, "=", cubed(num1))
+
+    if choice in ('7'):
+        try:
+            num1 = float(input("Enter the number you want to square root: "))
+        except ValueError:
+            print("Invalid input, Please enter a number.")
+            continue
+
+        if choice == '7':
+            print("The square root of", num1,"=", squareroot(num1))
+
+    if choice in ('8'):
+        try:
+            num1 = float(input("Enter the number you want to cube root: "))
+        except ValueError:
+            print("Invalid input, Please enter a number.")
+            continue
+
+        if choice == '8':
+            print("The cube root of", num1,"=", cuberoot(num1))
+
+    if choice in ('9'):
+        try:
+            num1 = float(input("Enter the number to check if it's prime: "))
+        except ValueError:
+            print("Invalid input, Please enter a number.")
+            continue
+
+        if choice == '9':
+            print(num1, isprime(num1))
+
+    if choice in ('10'):
+        try:
+            num1 = float(input("Enter the number you want to power: "))
+            num2 = float(input("Enter the number you want to power it by: "))
+        except ValueError:
+            print("Invalid input, Please enter a number.")
+            continue
+
+        if choice == '10':
+            print(num1, "to the power of", num2, "=", tothepowerof(num1, num2))
+            
+
+            
         
         
-        next_calculation = input("Do you want to do another calculation? (Yes/No): ")
-        if next_calculation == "no":
+    next_calculation = input("Do you want to do another calculation? (Yes/No): ")
+    if next_calculation == "no":
           break
     else:
         print("Invalid Input")
+
+
